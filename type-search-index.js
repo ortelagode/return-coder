03898f4e801c32763d7a5daa5b65d2ef74368a75
Ortela Gode <godeortela@gmail.com>
2022-01-17 17:34:28 +0100

    updates

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.model","l":"Death"},{"p":"com.example.controllers","l":"DeathController"},{"p":"com.example.app","l":"ServletLoader"},{"p":"com.example.util","l":"Util"},{"p":"com.example.model","l":"Villager"},{"p":"com.example.model","l":"VillagersRequest"}]
